c47c1449536a4cdb3221b16ee0d345ee553a5ca0
demi <idd90i@gmail.com>
2015-09-03 15:22:37 +0300

    Format code

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -7,8 +7,10 @@ package ru.javawebinar.topjava;
  * @link http://caloriesmng.herokuapp.com/
  * @link https://github.com/JavaOPs/topjava
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
-    }
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.format("Hello Topjava Enterprise!");
+	}
 }
