b5e8677b3319f76fd328036d467e6c2e5aeb0f2e
demi <idd90i@gmail.com>
2015-09-21 19:55:40 +0300

    Lesson 02

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -6,33 +6,57 @@ import java.time.LocalDateTime;
  * GKislin
  * 11.01.2015.
  */
-public class UserMeal
-{
+public class UserMeal {
+	protected Integer id;
+
 	protected final LocalDateTime dateTime;
 
 	protected final String description;
 
 	protected final int calories;
 
-	public UserMeal(LocalDateTime dateTime, String description, int calories)
-	{
+	public UserMeal(LocalDateTime dateTime, String description, int calories) {
+		this(null, dateTime, description, calories);
+	}
+
+	public UserMeal(Integer id, LocalDateTime dateTime, String description, int calories) {
+		this.id = id;
 		this.dateTime = dateTime;
 		this.description = description;
 		this.calories = calories;
 	}
 
-	public LocalDateTime getDateTime()
-	{
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDateTime() {
 		return dateTime;
 	}
 
-	public String getDescription()
-	{
+	public String getDescription() {
 		return description;
 	}
 
-	public int getCalories()
-	{
+	public int getCalories() {
 		return calories;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public boolean isNew() {
+		return id == null;
+	}
+
+	@Override
+	public String toString() {
+		return "UserMeal{" +
+				"id=" + id +
+				", dateTime=" + dateTime +
+				", description='" + description + '\'' +
+				", calories=" + calories +
+				'}';
 	}
 }
--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -6,8 +6,9 @@ import java.time.LocalDateTime;
  * GKislin
  * 11.01.2015.
  */
-public class UserMealWithExceed
-{
+public class UserMealWithExceed {
+	protected Integer id;
+
 	protected final LocalDateTime dateTime;
 
 	protected final String description;
@@ -16,19 +17,43 @@ public class UserMealWithExceed
 
 	protected final boolean exceed;
 
-	public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed)
-	{
+	public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+		this(null, dateTime, description, calories, exceed);
+	}
+
+	public UserMealWithExceed(Integer id, LocalDateTime dateTime, String description, int calories, boolean exceed) {
+		this.id = id;
 		this.dateTime = dateTime;
 		this.description = description;
 		this.calories = calories;
 		this.exceed = exceed;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public boolean isExceed() {
+		return exceed;
+	}
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "UserMealWithExceed{" +
-				"dateTime=" + dateTime +
+				"id=" + id +
+				", dateTime=" + dateTime +
 				", description='" + description + '\'' +
 				", calories=" + calories +
 				", exceed=" + exceed +
